c6245a77beb0b85110176385b3053bcf9f3ff4cb
richard.thompson@kantar.com <richard.thompson@kantar.com>
2017-04-20 07:12:45 -0500

    added

---
+++ InterviewWorkSolution/InterviewWork/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting...");
+
+            DoTheThing();
+
+            Console.WriteLine("Hit Enter to exit...");
+            Console.ReadLine();
+        }
+
+        private static void DoTheThing()
+        {
+            var n = 99;
+            var result = FibonacciCalc.GetFibonacciSequence(n);
+            Console.WriteLine(String.Format("First {0} Fibonaccis:", n));
+            Console.WriteLine(String.Join(",", result));
+        }
+    }
+}
