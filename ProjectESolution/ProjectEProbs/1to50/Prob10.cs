909e95e09d537ee8e093345d8407a253face599e
richard.thompson@kantar.com <richard.thompson@kantar.com>
2017-06-19 08:42:37 -0500

    finished problem 10

---
+++ ProjectESolution/ProjectEProbs/1to50/Prob10.cs
@@ -0,0 +1,26 @@
+﻿/*
+Summation of primes
+Problem 10 
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEProbs._1to50
+{
+    public static class Prob10
+    {
+        public static long SumOfPrimes(int n)
+        {
+            
+            var primes = OptimusPrime.Primes(n);            
+            long sum = primes.Sum();
+            return sum;
+        }
+    }
+}
---
+++ ProjectESolution/ProjectETests/Probs1to50/Prob10Tests.cs
@@ -0,0 +1,67 @@
+﻿/*
+Summation of primes
+Problem 10 
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ProjectEProbs;
+using ProjectEProbs._1to50;
+using ProjectETests.Utilities;
+
+namespace ProjectETests.Probs1to50
+{
+    [TestFixture]
+    [Category("Problem 10")]
+    public class Prob10Tests
+    {
+        [Test]
+        public void TestSumOfPrimes10()
+        {
+            int n = 10;
+            long solution = GetSum(n);
+            Assert.AreEqual(17, solution);
+            TestUtils.WriteOut($"Sum of first {n} primes is: {solution}");
+            
+        }
+
+        [Test]
+        public void TestSumOfPrimes100()
+        {
+            int n = 100;
+            long solution = GetSum(n);
+            TestUtils.WriteOut($"Sum of first {n} primes is: {solution}");
+            
+        }
+
+        [Test]
+        public void TestSumOfPrimes10000()
+        {
+            int n = 10000;
+            long solution = GetSum(n);
+            TestUtils.WriteOut($"Sum of first {n} primes is: {solution}");
+            
+        }
+
+        [Test]
+        public void TestSumOfPrimes2000000()
+        {
+            int n = 2000000;
+            long solution = GetSum(n);
+            TestUtils.WriteOut($"Sum of first {n} primes is: {solution}");
+            Assert.AreEqual(142913828922, solution);    
+        }
+
+        private long GetSum(int n)
+        {
+            return Prob10.SumOfPrimes(n);
+        }
+    }
+        
+}
