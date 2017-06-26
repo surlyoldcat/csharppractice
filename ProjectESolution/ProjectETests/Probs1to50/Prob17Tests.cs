225960ff05bd2d3a3744b402962b6e77858fa6ae
Richard Thompson <Richard.Thompson@kantar.com>
2017-06-26 14:01:32 -0500

    wip

---
+++ ProjectESolution/ProjectETests/Probs1to50/Prob17Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using ProjectEProbs._1to50;
+
+namespace ProjectETests.Probs1to50
+{
+    [TestFixture]
+    [Category("Problem 17")]
+    public class Prob17Tests
+    {
+        [Test]
+        public void TestSolution()
+        {
+            Assert.Fail();
+        }
+    }
+}
