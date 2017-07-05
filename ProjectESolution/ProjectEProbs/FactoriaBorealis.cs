fef6b92eb3c141eaf5456e640dd7182d0f2277be
richard.thompson@kantar.com <richard.thompson@kantar.com>
2017-07-05 09:32:06 -0500

    wip

---
+++ ProjectESolution/ProjectEProbs/FactoriaBorealis.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEProbs
+{
+    public static class FactoriaBorealis
+    {
+        public static BigInteger Factorial(long n)
+        {
+            if (n < 3) { return n * 1; }
+            else if (n == 3) { return 6; }
+            else if (n == 4) { return 24; }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+            
+        }
+    }
+}
