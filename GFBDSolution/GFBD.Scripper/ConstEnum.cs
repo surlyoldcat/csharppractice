c1fc446a75dce6d8dd77ca0b35393785eebbf34f
Richard Thompson <Richard.Thompson@kantar.com>
2017-07-06 13:55:48 -0500

    working on problem 2

---
+++ GFBDSolution/GFBD.Scripper/ConstEnum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GFBD.Scripper
+{
+    public enum OutputFormat
+    {
+        Html,
+        Json
+    }
+
+    public static class ConstEnum
+    {
+        
+
+
+    }
+}
---
+++ GFBDSolution/GFBD.Scripper/ObjectScripper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GFBD.Scripper
+{
+    
+
+    public class ObjectScripper
+    {
+        public ObjectScripper(object obj)
+        {
+            /*
+            1. get base type (simple/scalar or collection (or composite?))
+            2. build up a hierarchy of Thingys that matches the object's structure
+            3. recursively populate the hierarchy- it stops when it reaches
+                    a simple type;
+            4. call Render on the 'root.' this should trigger a recursive Render all the way down.
+            */
+        }
+
+       
+    }
+}
